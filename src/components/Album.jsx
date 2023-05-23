7888b95758b0052c0310814ea40743abbd322f62
Vincius Santos <viniciusantos300@gmail.com>
2023-05-23 00:06:13 -0300

    Requisitos de 1 a 3 completos(2)

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 class Album extends React.Component {
   render() {
-    return (<>
-      <div data-testid="page-album" />
-      <h1>Pagina de albuns</h1>
-            </>
+    return (
+      <>
+        <div data-testid="page-album" />
+        <h1>Pagina de albuns</h1>
+      </>
     );
   }
 }
